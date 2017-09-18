c1f66c7ab99891f05ce0079215a241d982591684
Guilherme Pires Altmann <altmannguilherme@gmail.com>
2017-09-18 20:28:47 -0300

    commit

--- NotasDiarias/NotasDiarias/AnotacaoViewController.swift
+++ NotasDiarias/NotasDiarias/AnotacaoViewController.swift
@@ -7,29 +7,77 @@
 //
 
 import UIKit
+import CoreData
 
 class AnotacaoViewController: UIViewController {
 
+    @IBOutlet weak var texto: UITextView!
+    var context: NSManagedObjectContext!
+    var anotacao: NSManagedObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.texto.becomeFirstResponder()
+        
+        if self.anotacao != nil{
+            
+            self.texto.text = anotacao.value(forKey: "texto") as? String
+        }else{
+            
+            self.texto.text = ""
+        }
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        self.context = appDelegate.persistentContainer.viewContext
     }
-
+    
+    @IBAction func salvar(_ sender: Any) {
+        
+        if self.anotacao != nil{
+            
+            self.atualizarAnotacao()
+        }else{
+            
+            self.salvarAnotacao()
+        }
+        
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    func atualizarAnotacao(){
+        
+        self.anotacao.setValue(self.texto.text, forKey: "texto")
+        self.anotacao.setValue(Date(), forKey: "data")
+        
+        do {
+            
+            try self.context.save()
+        } catch let erro as Error {
+            
+            print("Erro ao salvar : \(erro.localizedDescription)")
+        }
+    }
+    
+    func salvarAnotacao() {
+        
+        let novaAnotacao = NSEntityDescription.insertNewObject(forEntityName: "Anotacao", into: self.context)
+        
+        novaAnotacao.setValue(self.texto.text, forKey: "texto")
+        novaAnotacao.setValue(Date(), forKey: "data")
+        
+        do {
+            
+            try self.context.save()
+        } catch let erro as Error {
+            
+            print("Erro ao salvar : \(erro.localizedDescription)")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- NotasDiarias/NotasDiarias/ListarAnotacoesTableViewController.swift
+++ NotasDiarias/NotasDiarias/ListarAnotacoesTableViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import CoreData
 
 class ListarAnotacoesTableViewController: UITableViewController {
 
+    var context: NSManagedObjectContext!
+    var anotacoes: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        self.context = appDelegate.persistentContainer.viewContext
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.recuperarAnotacaoes()
+    }
+    
+    func recuperarAnotacaoes(){
+     
+        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Anotacao")
+    
+        do {
+            
+            let anotacoesRecuperadas = try self.context.fetch(requisicao)
+    
+            self.anotacoes = anotacoesRecuperadas as! [NSManagedObject]
+            self.tableView.reloadData()
+        } catch let erro as Error {
+            print("Erro ao recuoerar dados : \(erro.localizedDescription)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,67 +50,46 @@ class ListarAnotacoesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.anotacoes.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
 
-        // Configure the cell...
-
+        let anotacao = self.anotacoes[indexPath.row]
+        cell.textLabel?.text = anotacao.value(forKey: "texto") as? String
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy hh:mm"
+        
+        let novaData = dateFormatter.string(from: anotacao.value(forKey: "data") as! Date)
+        
+        cell.detailTextLabel?.text = String(describing: novaData)
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        
+        let anotacao = self.anotacoes[indexPath.row]
+        
+        self.performSegue(withIdentifier: "verAnotacao", sender: anotacao)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "verAnotacao"{
+            
+            let viewDestino = segue.destination as! AnotacaoViewController
+            viewDestino.anotacao = sender as? NSManagedObject
+            
+        }
     }
-    */
-
 }
